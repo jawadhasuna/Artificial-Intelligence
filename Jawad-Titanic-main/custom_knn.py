b985f422e72213dff758255c61290e2b6b10d2f9
jawadhasuna <171467245+jawadhasuna@users.noreply.github.com>
2025-12-12 05:41:11 +0500

    Update custom_knn.py

--- Jawad-Titanic-main/custom_knn.py
+++ Jawad-Titanic-main/custom_knn.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def euclidean_distance(a, b):
     return np.sqrt(np.sum((a - b) ** 2))
@@ -6,16 +7,13 @@ def euclidean_distance(a, b):
 class CustomKNN:
     def __init__(self, k=3):
         self.k = k
-        self.X_train = None
-        self.y_train = None
 
     def fit(self, X, y):
-        self.X_train = X
-        self.y_train = y
+        self.X_train = X.values
+        self.y_train = y.values
 
     def predict_one(self, x):
         distances = []
-
         for i in range(len(self.X_train)):
             dist = euclidean_distance(self.X_train[i], x)
             distances.append((dist, self.y_train[i]))
@@ -25,11 +23,11 @@ class CustomKNN:
 
         labels = [label for _, label in k_neighbors]
         prediction = max(set(labels), key=labels.count)
-
         return prediction
 
     def predict(self, X):
         predictions = []
-        for i in range(len(X)):
-            predictions.append(self.predict_one(X[i]))
+        X_np = X.values  # avoids KeyError
+        for i in range(len(X_np)):
+            predictions.append(self.predict_one(X_np[i]))
         return np.array(predictions)
